3a599d9c363e495d4b8ce6e1227b39d93fe99286
ConyAB <cbazua@uc.cl>
2024-10-16 20:53:29 -0300

    tablas listas

--- cargador/cargador.php
+++ cargador/cargador.php
@@ -1,26 +1,15 @@
 <?php
 
-// Definición de las variables de conexión a la base de datos
-$dbname = "grupo79";         // Nombre de la base de datos
-$user = "grupo79";           // Nombre de usuario para la conexión
-$password = "grupo79";       // Contraseña para la conexión
+$dbname = "grupo79";
+$user = "grupo79";
+$password = "grupo79";
 
-// Establecer la conexión a la base de datos
 $conn = pg_connect("host=localhost port=22 dbname=$dbname user=$user password=$password");
 if (!$conn) {
-    die("Connection failed: " . pg_last_error()); 
+    die("Connection failed: " . pg_last_error());
 }
 
-function crear_tabla_personas($conn) {
-    $sql = "CREATE TABLE IF NOT EXISTS personas (
-        id_persona SERIAL PRIMARY KEY,       
-        nombre VARCHAR(100) NOT NULL,   
-        correo VARCHAR(100),
-        telefono VARCHAR(9),    
-        RUN VARCHAR(10) UNIQUE NOT NULL,
-        DV CHAR(1) NOT NULL,
-        estamento VARCHAR(20) DEFAULT 'NO VIGENTE' CHECK (estamento IN ('VIGENTE', 'EXALUMNO', 'NO VIGENTE'))
-    )";
+function ejecutar_query($conn, $sql) {
     $result = pg_query($conn, $sql);
     if ($result) {
         echo "Tabla creada correctamente\n";
@@ -29,82 +18,61 @@ function crear_tabla_personas($conn) {
     }
 }
 
-function crear_tabla_trabajador($conn) {
-
-    $sql = "CREATE TABLE IF NOT EXISTS trabajadores (
-        id_trabajador,
-        id_persona,
-        cargo,
-        dedicacion,
-        contrato,
-        grado_academico
+function crear_tabla_personas($conn) {
+    $sql = "CREATE TABLE IF NOT EXISTS personas (
+        id_persona SERIAL PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        correo VARCHAR(100),
+        telefono VARCHAR(9),
+        RUN VARCHAR(10) UNIQUE NOT NULL,
+        DV CHAR(1) NOT NULL,
+        estamento VARCHAR(20) DEFAULT 'NO VIGENTE' 
+            CHECK (estamento IN ('VIGENTE', 'EXALUMNO', 'NO VIGENTE'))
     )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";  
-    } else {
-        echo "Error creando la tabla: " . pg_last_error($conn) . "\n";  
-    }
+    ejecutar_query($conn, $sql);
 }
 
-function crear_tabla_estudiante($conn) {
-    
+
+function crear_tabla_trabajadores($conn) {
+    $sql = "CREATE TABLE IF NOT EXISTS trabajadores (
+        id_trabajador SERIAL PRIMARY KEY,
+        id_persona INT REFERENCES personas(id_persona),
+        cargo VARCHAR(50) NOT NULL,
+        dedicacion INT CHECK (dedicacion <= 40),
+        contrato VARCHAR(20) CHECK (contrato IN ('FULL TIME', 'PART TIME', 'HONORARIO')),
+        grado_academico VARCHAR(50) CHECK (grado_academico IN ('LICENCIATURA', 'MAGISTER', 'DOCTOR'))
+    )";
+    ejecutar_query($conn, $sql);
+}
+
+function crear_tabla_estudiantes($conn) {
     $sql = "CREATE TABLE IF NOT EXISTS estudiantes (
         id_estudiante SERIAL PRIMARY KEY,
-        id_persona,
-        numero_estudiante,
-        cohorte,
-        estado_bloqueo,
-        ultimo_logro,
-        ultima_carga
+        id_persona INT REFERENCES personas(id_persona),
+        numero_estudiante VARCHAR(15) UNIQUE NOT NULL,
+        cohorte VARCHAR(7),
+        estado_bloqueo BOOLEAN DEFAULT FALSE,
+        ultimo_logro VARCHAR(50),
+        ultima_carga VARCHAR(7)
     )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";  
-    } else {
-        echo "Error creando la tabla: " . pg_last_error($conn) . "\n";  
-    }
+    ejecutar_query($conn, $sql);
 }
 
-function crear_tabla_carrera($conn) {
-    $sql = "CREATE TABLE IF NOT EXISTS carreras (
-        id_carrera,
-        nombre
-    )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
-}
 
 function crear_tabla_planes_estudio($conn) {
     $sql = "CREATE TABLE IF NOT EXISTS planes_estudio (
         id_plan SERIAL PRIMARY KEY,
-        id_carrera INTEGER REFERENCES carreras(id_carrera) ON DELETE CASCADE,
+        id_carrera INT REFERENCES carreras(id_carrera) ON DELETE CASCADE,
         codigo_plan VARCHAR(10) UNIQUE NOT NULL,
         fecha_inicio DATE NOT NULL,
-        jornada VARCHAR(10) NOT NULL CHECK (jornada IN ('DIURNA', 'VESPERTINA')),
-        modalidad VARCHAR(10) NOT NULL CHECK (modalidad IN ('PRESENCIAL', 'ONLINE', 'HIBRIDA')),
-        sede VARCHAR(20) NOT NULL CHECK (sede IN ('HOGWARTS', 'BEAUXBATON', 'UAGADOU')),
-        nombre_plan VARCHAR(20) NOT NULL CHECK (sede IN ('HOGWARTS', 'BEAUXBATON'),
-        grado_plan VARCHAR(50) NOT NULL CHECK (grado_plan IN 'PROGRAMA ESPECIAL DE LICENCIATURA', 'PREGRADO', 'POSTGRADO'))
+        jornada VARCHAR(10) CHECK (jornada IN ('DIURNA', 'VESPERTINA')),
+        modalidad VARCHAR(10) CHECK (modalidad IN ('PRESENCIAL', 'ONLINE', 'HIBRIDA')),
+        sede VARCHAR(20) CHECK (sede IN ('HOGWARTS', 'BEAUXBATON', 'UAGADOU')),
+        nombre_plan VARCHAR(100) NOT NULL,
+        grado_plan VARCHAR(50) 
+            CHECK (grado_plan IN ('PROGRAMA ESPECIAL DE LICENCIATURA', 'PREGRADO', 'POSTGRADO'))
     )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
+    ejecutar_query($conn, $sql);
 }
 
 function crear_tabla_cursos($conn) {
@@ -113,106 +81,36 @@ function crear_tabla_cursos($conn) {
         sigla VARCHAR(10) UNIQUE NOT NULL,
         nombre VARCHAR(255),
         caracter VARCHAR(50),
-        nivel INT,
-        ciclo VARCHAR(10)
+        nivel INT CHECK (nivel >= 1 AND nivel <= 10),
+        ciclo INT CHECK (ciclo >= 1 AND nivel <= 10)
     )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
+    ejecutar_query($conn, $sql);
 }
 
 function crear_tabla_historial_academico($conn) {
     $sql = "CREATE TABLE IF NOT EXISTS historiales_academicos (
         id_historial SERIAL PRIMARY KEY,
-        id_estudiante SERIAL,
-        id_curso,
-        nota,
-        calificacion,
-        descripcion
+        id_estudiante INT REFERENCES estudiantes(id_estudiante),
+        id_curso INT REFERENCES cursos(id_curso),
+        nota NUMERIC (3,1) CHECK (nota >= 1.0 AND nota <= 7.0),
+        calificacion VARCHAR(2) CHECK (calificacion IN ('SO', 'MB', 'B', 'SU', 'I', 'M', 'MM', 'P', 'NP', 'EX', 'A', 'R')),
+        descripcion TEXT
     )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
-}
-
-function crear_tabla_departamentos($conn) {
-    $sql = "CREATE TABLE IF NOT EXISTS departamentos (
-        codigo_departamento,
-        nombre_departamento
-    )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
-}
-
-function crear_tabla_facultades($conn) {
-    $sql = "CREATE TABLE IF NOT EXISTS facultades (
-        id_facultad,
-        nombre_facultad ,
-        codigo_departamento
-    )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
+    ejecutar_query($conn, $sql);
 }
 
 function crear_tabla_oferta_academica($conn) {
     $sql = "CREATE TABLE IF NOT EXISTS oferta_academica (
-        id_oferta ,
-        id_curso,
-        vacantes,
-        sala,
-        modulo_horario,
-        periodo,
-        seccion,
-        nombre_profesor
+        id_oferta SERIAL PRIMARY KEY,
+        id_curso INT REFERENCES cursos(id_curso),
+        vacantes INT CHECK (vacantes > 0),
+        sala VARCHAR(50),
+        modulo_horario VARCHAR(50),
+        periodo VARCHAR(7),
+        seccion VARCHAR(10),
+        nombre_profesor VARCHAR(100) DEFAULT 'POR DESIGNAR'
     )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
-}
-
-function crear_tabla_prerrequisitos($conn) {
-    $sql = "CREATE TABLE IF NOT EXISTS prerrequisitos (
-        id_prerrequisito,
-        id_curso,
-        sigla,
-        ciclo
-    )";
-
-    $result = pg_query($conn, $sql);
-
-    if ($result) {
-        echo "Tabla creada correctamente\n";
-    } else {
-        echo "Error creando la tabla: ".pg_last_error($conn) . "\n";
-    }
+    ejecutar_query($conn, $sql);
 }
 
 ?>
-
